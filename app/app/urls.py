6411cb930dc76c592344b81869b193b0a86b5c06
veekthor04 <victoradenuga04@yahoo.com>
2021-12-06 10:08:15 +0100

    heroku build

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,13 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import url
-from django.conf import settings
-from django.views.static import serve
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('portfolio.urls'), name='portfolio'),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    path('', include('portfolio.urls'), name='portfolio')
 ]
